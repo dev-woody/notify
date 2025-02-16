6ef3e8c4d6716bdf700c5685fd25f257d172c6fa
woody <w_odyy@icloud.com>
2025-02-17 04:01:49 +0900

    config

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
         domains: ["dummyimage.com", "via.placeholder.com"], // 외부 이미지 허용
-
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
